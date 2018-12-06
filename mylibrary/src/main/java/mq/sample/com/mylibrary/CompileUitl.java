f604a6b962a46b1aad5989e98ea6230cfbac2eca
pengjiajia2018 <18380460383@126.com>
2018-12-06 18:18:36 +0800

    1.代码加入

---
+++ mylibrary/src/main/java/mq/sample/com/mylibrary/CompileUitl.java
@@ -0,0 +1,11 @@
+package mq.sample.com.mylibrary;
+
+/**
+ * Created by Administrator on 2018/12/6.
+ */
+
+public class CompileUitl {
+    public static int doAdd(int a, int b) {
+        return a + b;
+    }
+}
